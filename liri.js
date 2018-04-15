c2c92b2c4e1e19725b71acc8314bd979952f238c
Allen Welch <allenjwelch@gmail.com>
2018-04-15 15:35:18 -0400

    twitter, spotify, & ombd working successfully

--- liri.js
+++ liri.js
@@ -1,78 +1,196 @@
+// NPM PACKAGES
 require("dotenv").config();
-
 const keys = require("./keys.js");
 const figlet = require('figlet');
+const Twitter = require('twitter');
+const Spotify = require('node-spotify-api');
+const request = require('request');
 const fs = require('fs'); 
 // EX: fs.readFile('bank.txt', 'utf8', (err,data) => {
 
+// USER INPUTS
 const liriCmd = process.argv[2]; 
-const songName = process.argv[3]; 
-const movieName = process.argv[3]; 
-  
-// const spotify = new Spotify(keys.spotify);
-const client = keys.twitter;
-const ombdURL = `http://www.omdbapi.com/?apikey=trilogy&s=${movieName}`; 
+let songName = process.argv; 
+let movieName = process.argv; 
+  movieName.splice(0,3); 
 
-console.log(client); 
+
+// KEYS 
+const spotify = new Spotify(keys.spotify); 
+const client = new Twitter(keys.twitter);
+
+// console.log(client.options); 
+// console.log(spotify.credentials);
 
 switch (liriCmd) {
   case 'my-tweets':
-    // code; 
+    twitterGet(); 
     break; 
   case 'spotify-this-song':
-    // code; 
-    break; 
+    if (typeof songName[0] === 'undefined') {
+      spotifyGetAoB(); 
+    } else {
+      var songNameStr = songName.join(' '); 
+      spotifyGet(); 
+    }
+      break; 
   case 'movie-this':
-    // code; 
+    if (typeof movieName[0] === 'undefined') {
+      var movieNameStr = 'Mr.Nobody'; 
+      omdbGet();  
+    } else {
+      var movieNameStr = movieName.join(' '); 
+      omdbGet(); 
+    }
     break; 
   case 'do-what-it-says':
     // code; 
+    doItFig(); 
+    // setTimeout(() => {
+
+    // }, 500); 
     break; 
 }
-
-// 1. `node liri.js my-tweets`
-//    * This will show your last 20 tweets and when they were created at in your terminal/bash window.
-
-// 2. `node liri.js spotify-this-song '<song name here>'`
-//    * This will show the following information about the song in your terminal/bash window
-//      * Artist(s)
-//      * The song's name
-//      * A preview link of the song from Spotify
-//      * The album that the song is from
-//    * If no song is provided then your program will default to "The Sign" by Ace of Base.
-
-// 3. `node liri.js movie-this '<movie name here>'`
-//    * This will output the following information to your terminal/bash window:
-
-//      ```
-//        * Title of the movie.
-//        * Year the movie came out.
-//        * IMDB Rating of the movie.
-//        * Rotten Tomatoes Rating of the movie.
-//        * Country where the movie was produced.
-//        * Language of the movie.
-//        * Plot of the movie.
-//        * Actors in the movie.
-//      ```
-//    * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
 
 // 4. `node liri.js do-what-it-says`
 //    * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
 
 
+function omdbGet() {
+  omdbFig(); 
+  const omdbURL = `http://www.omdbapi.com/?apikey=trilogy&t=${movieNameStr}`;
+  setTimeout(() => {
+    request(omdbURL, function(err, response, body) {
+      if (!err && response.statusCode === 200) {
+        const data = JSON.parse(body);
+        console.log('\n====================')
+        console.log(`Title: ${data.Title}`);
+        console.log(`Year: ${data.Year}`);
+        console.log(`IMDB Rating: ${data.imdbRating}`);
+        console.log(`Rotten Tomatoes Rating: ${data.Ratings[1].Value}`);
+        console.log(`Country: ${data.Country}`);
+        console.log(`Language: ${data.Language}`);
+        console.log(`Actors: ${data.Actors}`);
+        console.log(`Plot: ${data.Plot}`);
+        console.log('====================')
+      }; 
+    });
+  }, 500); 
+} 
 
-// figlet('Figlet!!', {
-//   font: 'doh',
-//   horizontalLayout: 'default',
-//   verticalLayout: 'default'
-// }, function(err, data) {
-//     if (err) {
-//         console.log('Something went wrong...');
-//         console.dir(err);
-//         return;
-//     }
-//     console.log(data)
-// });
+function spotifyGet() {
+  spotifyFig(); 
+    setTimeout(() => {
+      spotify.search({ type: 'track', query: songNameStr }, function(err, data) {
+        if(err) throw err; 
+        // console.log(data.tracks.items[0]); 
+        console.log('\n====================')
+        console.log(`Artist: ${data.tracks.items[0].artists[0].name}`);
+        console.log(`Track: ${data.tracks.items[0].name}`); 
+        console.log(`Album: ${data.tracks.items[0].album.name}`);
+        console.log(`Link: ${data.tracks.items[0].album.external_urls.spotify}`);
+        console.log('====================')
+      });
+    }, 500); 
+}
+
+function spotifyGetAoB() {
+  spotifyFig(); 
+      setTimeout(() => {
+      spotify.search({ type: 'track', query: 'the sign ace of base' }, function(err, data) {
+        if(err) throw err; 
+        // console.log(data.tracks.items[0]); 
+        console.log('\nNeed a suggestion? How about...'); 
+        console.log('\n====================')
+        console.log(`Artist: ${data.tracks.items[0].artists[0].name}`);
+        console.log(`Track: ${data.tracks.items[0].name}`); 
+        console.log(`Album: ${data.tracks.items[0].album.name}`);
+        console.log(`Link: ${data.tracks.items[0].album.external_urls.spotify}`);
+        console.log('====================')
+      });
+    }, 500); 
+}
+
+function twitterGet() {
+  // client.get(path, params, callback);
+  tweetFig(); 
+  setTimeout(() =>{
+    client.get('https://api.twitter.com/1.1/favorites/list.json', function(err, tweets, response) {
+      if(err) throw err;
+      tweets.forEach(tweet => {
+        console.log('\n====================')
+        console.log(`Date: ${tweet.created_at}`);  // Statuses 
+        console.log(`\n${tweet.text}`);  // Statuses 
+        console.log('====================')
+      })
+    });
+  }, 500);
+}
+
+function tweetFig() {
+  figlet('Twitter', {
+    font: 'big',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+  }, function(err, data) {
+    if (err) {
+      console.log('Something went wrong...');
+      console.dir(err);
+      return;
+    }
+    console.log(data)
+    console.log('"Liri, what the **** is twitter?"')
+  });
+}
+
+function spotifyFig() {
+  figlet('Spotify', {
+    font: 'big',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+  }, function(err, data) {
+    if (err) {
+      console.log('Something went wrong...');
+      console.dir(err);
+      return;
+    }
+    console.log(data)
+    console.log('"Liri, I\'d like some music to set the mood."')
+
+  });
+}
+
+function omdbFig() {
+  figlet('OMDB', {
+    font: 'big',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+  }, function(err, data) {
+    if (err) {
+      console.log('Something went wrong...');
+      console.dir(err);
+      return;
+    }
+    console.log(data)
+    console.log('"Liri, what\'s on for movie night?"')
+
+  });
+}
+
+function doItFig() {
+  figlet('Do it!', {
+    font: 'big',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+  }, function(err, data) {
+    if (err) {
+      console.log('Something went wrong...');
+      console.dir(err);
+      return;
+    }
+    console.log(data)
+  });
+}
 
 
 // module.exports = {
